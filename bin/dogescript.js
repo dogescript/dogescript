d2e3e468af844c6cafb393e82ab86eaef65e5bdf
Jacob Groundwater <groundwater@gmail.com>
2013-11-11 23:52:46 -0800

    add REPL

--- bin/dogescript.js
+++ bin/dogescript.js
@@ -2,11 +2,14 @@
 
 var fs       = require('fs');
 var path     = require('path');
-var argv     = require('optimist').usage('Usage: dogescript <file>').demand(1).argv;
+var util     = require('util');
+var stream   = require('stream');
+var repl     = require('repl');
+var argv     = require('optimist').usage('Usage: dogescript <file>').argv;
 var beautify = require('js-beautify').js_beautify;
-
 var parser   = require('../lib/parser');
 
+if (argv._[0]){
 var file = fs.readFile(path.resolve(process.cwd(), argv._[0]), {encoding: 'utf-8'}, function (err, script) {
     var lines = script.split('\n');
     var output = '';
@@ -18,3 +21,37 @@ var file = fs.readFile(path.resolve(process.cwd(), argv._[0]), {encoding: 'utf-8
     if (argv.beautify) process.stdout.write(beautify(output))
     else process.stdout.write(output);
 });
+}
+else {
+
+  // streamy inheritance stuff
+  // boilerblate from the docs
+  util.inherits(DogeJSStream, stream.Transform);
+  function DogeJSStream(){
+    stream.Transform.call(this);
+  }
+  
+  // see streams documentation
+  DogeJSStream.prototype._transform = function(chunk, encoding, next){
+    var script = parser( chunk.toString() );
+    var lines  = script.split(/\n+/);
+    for(var i=0; i<lines.length; i++) {
+      // ignore empty lines
+      if(lines[i]!=='') this.push( lines[i] + '\n' );
+    }
+    next();
+  }
+
+  var ds = new DogeJSStream();
+
+  // pipe stdin through the dogescript tranlator to the repl
+  repl.start({
+    prompt : "DOGE> ",
+    input  : ds,
+    output : process.stdout
+  });
+
+  // begin streaming stdin to the dg translator and repl
+  process.stdin.pipe(ds);
+ 
+}
