807fe22470a9b7330bb2811704d531fdf073be24
remixz <zbruggeman@me.com>
2013-11-15 23:07:21 -0800

    Clean up

--- bin/dogescript.js
+++ bin/dogescript.js
@@ -9,7 +9,7 @@ var argv     = require('optimist').usage('Usage: dogescript <file>').argv;
 var beautify = require('js-beautify').js_beautify;
 var parser   = require('../lib/parser');
 
-if (argv._[0]){
+if (argv._[0]) {
   var file = fs.readFile(path.resolve(process.cwd(), argv._[0]), {encoding: 'utf-8'}, function (err, script) {
       var lines = script.split('\n');
       var output = '';
@@ -21,37 +21,33 @@ if (argv._[0]){
       if (argv.beautify) process.stdout.write(beautify(output))
       else process.stdout.write(output);
   });
-}
-else {
-
-  // streamy inheritance stuff
-  // boilerblate from the docs
-  util.inherits(DogeJSStream, stream.Transform);
-  function DogeJSStream(){
-    stream.Transform.call(this);
-  }
-  
-  // see streams documentation
-  DogeJSStream.prototype._transform = function(chunk, encoding, next){
-    var script = parser( chunk.toString() );
-    var lines  = script.split(/\n+/);
-    for(var i=0; i<lines.length; i++) {
-      // ignore empty lines
-      if(lines[i]!=='') this.push( lines[i] + '\n' );
+} else {
+    // streamy inheritance stuff
+    // boilerblate from the docs
+    function Stream () {
+        stream.Transform.call(this);
     }
-    next();
-  }
+    util.inherits(Stream, stream.Transform);
+      
+    // see streams documentation
+    Stream.prototype._transform = function (chunk, encoding, callback) {
+        var script = parser(chunk.toString());
+        var lines  = script.split(/\n+/);
+        for (var i = 0; i < lines.length; i++) {
+            // ignore empty lines
+            if (lines[i] !== '') this.push(lines[i] + '\n');
+        }
+        callback();
+    }
 
-  var ds = new DogeJSStream();
+    var ds = new Stream();
+    // pipe stdin through the dogescript translator to the repl
+    repl.start({
+        prompt : "DOGE> ",
+        input  : ds,
+        output : process.stdout
+    });
 
-  // pipe stdin through the dogescript tranlator to the repl
-  repl.start({
-    prompt : "DOGE> ",
-    input  : ds,
-    output : process.stdout
-  });
-
-  // begin streaming stdin to the dg translator and repl
-  process.stdin.pipe(ds);
- 
+    // begin streaming stdin to the dg translator and repl
+    process.stdin.pipe(ds);
 }
