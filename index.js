de85d27ffec8a607dbbc04c1e18c77a27c17a091
Bart van der Schoor <bartvanderschoor@gmail.com>
2014-06-07 22:19:36 +0200

    improved code editor
    added behave.js for better text editing
    - indenting (tab/ shift-tab)
    - stuff with braces etc
    
    tuned textarea size css & js
    - disabled manual resize
    - locked width at 49.9%
    - added javascript to keep maximum height
    
    switch classes to ID's

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 var dogescript = require('dogescript');
 
-var input  = document.getElementsByClassName('dogescript')[0];
-var output = document.getElementsByClassName('javascript')[0];
+var input  = document.getElementById('dogescript');
+var output = document.getElementById('javascript');
 
-input.addEventListener('keyup', function () {
+var editor = new Behave({
+    textarea: input,
+    replaceTab: true,
+    softTabs: true,
+    tabSize: 4,
+    autoOpen: true,
+    overwrite: true,
+    autoStrip: true,
+    autoIndent: true,
+    fence: false
+});
+
+BehaveHooks.add('keyup', function(data){
     output.value = dogescript(input.value, true);
 });
 
 output.value = dogescript(input.value, true);
+
+function fixSize() {
+    var top = input.getBoundingClientRect().top + document.body.scrollTop;
+    var height = (window.innerHeight - top - 10) + 'px';
+    input.style.height = height;
+    output.style.height = height; 
+}
+
+fixSize();
+
+var debounce = 0;
+
+window.addEventListener('resize', function(e) {
+    if (debounce) {
+        debounce = clearTimeout(debounce);
+    }
+    debounce = setTimeout(fixSize, 50);
+});
