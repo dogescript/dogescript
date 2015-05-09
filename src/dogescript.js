3312e71db982cab5147ac735d19832e26f97b939
Pholey <pholeyoh@gmail.com>
2015-05-09 05:11:32 -0700

    :art: Add error message

--- src/dogescript.js
+++ src/dogescript.js
@@ -3,6 +3,11 @@ import Promise from "bluebird"
 import {tokenizeFile} from "./tokenizer"
 
 export function main(dir, args) {
+  if (!args) {
+    console.log("Please select a file to parse")
+    return
+  }
+
   let path = dir + "/" + args
   tokenizeFile(path).then(tokens => {
     console.log(tokens.map(x => {return x.name}))
