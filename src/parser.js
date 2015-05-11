2198b7a3e8c482fbdb509135ab76ae260f1c18c7
Pholey <cassidy@launchbadge.com>
2015-05-11 12:29:53 -0700

    Work on tokens

--- src/parser.js
+++ src/parser.js
@@ -1,22 +1,38 @@
-class Parser {
+import R from "ramda";
+
+// Defines a collection of tokens, and saves any processed tokens to the buffer
+// Storing only the maximum amount of tokens needed for parsing (MAX_LOOKBEHIND)
+class Tokens {
+
+  MAX_LOOKBEHIND = 3
 
   constructor(lineNo, line) {
     this.line = line
     this.lineNo = lineNo
-    this.buffer = []
-    this.token = { peek() {
-                    return this.line[0]
-                   }
-                 , pop() {
-                      return this.line.pop()
-                    }
-                 , lookAhead(n) {
-                    return this.line[n]
-                   }
-                 , lookBehind(n) {
-                   return this.buffer[n]
-                   }
-                 }
+    this.lookBehindBuf = []
+    this.lineBuff = []
+  }
+
+  peek() {
+   return this.line[0]
+  }
+
+  pop() {
+   this.buffer.push(this.peek())
+   return this.line.pop()
+  }
+
+  lookAhead(n) {
+   return this.line[n]
+  }
+
+  lookBehind(n) {
+    return this.buffer[n]
+  }
+
+  setLine(line, lineNo) {
+    this.line = line
+    this.lineNo = lineNo
   }
 
 }
