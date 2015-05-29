e8d3bad64186febbf9333bc99bb62977836cac10
Pholey <pholeyoh@gmail.com>
2015-05-28 23:40:28 -0700

    :shirt:

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -3,23 +3,20 @@ import R from "ramda"
 import {mapOverLines} from "./reader"
 import Token from "./tokens"
 
-export var tokenizeIdentifiers = R.map(t => {
-
-  // The position of the token on the line
-  let pos = line.indexOf(t)
-
-  // Grab the token or default to an Identifier
-  let tok = new (Token[t] || Token.Identifier)
-
-  // Create and return the new token
-  return tok.create(pos,t)
-})
-
 function tokenizeLine(line) {
   // Blank empty newline -- do nothing
   if (line.length === 0) return
 
-  return tokenizeIdentifiers(line.split(" "))
+  return R.map(t => {
+    // The position of the token on the line
+    let pos = line.indexOf(t)
+
+    // Grab the token or default to an Identifier
+    let tok = new (Token[t] || Token.Identifier)
+
+    // Create and return the new token
+    return tok.create(pos,t)
+  }, line.split(" "))
 
 }
 
