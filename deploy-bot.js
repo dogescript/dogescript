add06f06c655a609c45460e28dca52bb6b4a8c28
AnEmortalKid <janmonterrubio@gmail.com>
2018-04-08 15:44:13 -0500

    add some loggers

--- deploy-bot.js
+++ deploy-bot.js
@@ -1,8 +1,10 @@
 var dotenv = require('dotenv');
 dotenv.load();
+console.log('Loaded env.');
 
-const Heroku = require('heroku-client')
-const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN })
+const Heroku = require('heroku-client');
+const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
 
 // restart the dyno
+console.log('Restarting dino.');
 heroku.delete('/apps/doge-eval-bot/dynos/web.1').then(app => {});
