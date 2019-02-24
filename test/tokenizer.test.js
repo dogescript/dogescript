342f3e332533f2d7a1923ae1b33c50977c79accc
Jan Monterrubio <janmonterrubio@gmail.com>
2019-02-23 23:49:51 -0600

    more tests

--- test/tokenizer.test.js
+++ test/tokenizer.test.js
@@ -20,3 +20,24 @@ test("handles escaped single quotes", function (t){
   var tokens = tokenizer.tokenize(input);
   t.deepEqual(tokens, ["plz", "foo", "with", "'\''"]);
 });
+
+test("handles json ", function (t){
+  t.plan(1);
+  var input = "very x is { foo: bar }";
+  var tokens = tokenizer.tokenize(input);
+  t.deepEqual(tokens, ["very", "x", "is", "{", "foo:", "bar", "}"]);
+});
+
+test("handles arrays", function (t){
+  t.plan(1);
+  var input = "very x is [1, 2, 3]";
+  var tokens = tokenizer.tokenize(input);
+  t.deepEqual(tokens, ["very", "x", "is", "[1,", "2,", "3]"]);
+});
+
+test("doens't break single quoted strings", function (t) {
+  t.plan(1);
+  var input = "'hello world!'";
+  var tokens = tokenizer.tokenize(input);
+  t.deepEqual(tokens, ["'hello world!'"]);
+});
