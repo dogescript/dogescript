540331d88c25fbd148e24a2d8bc70c50ac8f808e
AnEmortalKid <janmonterrubio@gmail.com>
2019-03-28 23:02:00 -0500

    address pr comments

--- test/transpile.test.js
+++ test/transpile.test.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var test = require('tape');
-var walk = require('walk');
+const { exec } = require('child_process');
 
 // deal with CRLF from windows folks writing javascript :(
 function readCleanCRLF(fpath) {
@@ -14,86 +14,38 @@ function cleanCRLF(str) {
   return str.trim().replace(/\r\n/gm, '\n');
 }
 
+function fetchExpected(testDirName)
+{
+  var expectedPath   = path.join(transpileDir, testDirName, 'expected.js');
+  return readCleanCRLF(expectedPath);
+}
+
+function fetchSource(testDirName)
+{
+  return path.join(transpileDir, testDirName, 'source.djs');
+}
+
 var transpileDir = path.join(__dirname, 'transpile');
 
-// Pop a directory off it's path (os safe)
-function dotdotSlash(filePath) {
-  var str;
-  if (filePath.indexOf('/') == -1) { // windows
-    str = filePath.substring(0, filePath.lastIndexOf('\\'));
-  }
-  else { // unix
-    str = filePath.substring(0, filePath.lastIndexOf('/'));
-  }
-
-  return str + '/'
-}
-
-function getFolderName(filePath) {
-  
-  // windows
-  if (filePath.indexOf('/') == -1) { 
-    return filePath.substring(filePath.lastIndexOf('\\') + 1, filePath.length);
-  }
-  
-  // unix
-  return filePath.substring(filePath.lastIndexOf('/') + 1, filePath.length);
-}
-
-var testDirs = {};
-
-// Who's your daddy and what does he do?
-var skywalker = walk.walk(transpileDir);
-
-// Walk every folder in the file tree adding any folders that;
-// have an expected.js and have a source.js
-skywalker.on('names', function(fpath, children) {
-  var missingExpectFile = children.indexOf('expected.js') === -1;
-  var missingSourceFile = children.indexOf('source.djs') === -1;
-
-  
-  if(missingExpectFile || missingSourceFile) return;
-  
-  testDirs[fpath] = { missingExpectFile, missingSourceFile };
-});
-
-skywalker.on('end', function() {
-
-  var keys = Object.keys(testDirs);
-  keys.forEach(dir => {
-
-    var expectPath   = path.join(dir, 'expected.js');
-    var sourcePath     = path.join(dir, 'source.djs');
-    var expected = readCleanCRLF(expectPath);
-
+function runTest(testDirName)
+{
+  test(`${testDirName} should transpile correctly`, function (t) {
+    t.plan(1);
+    var expected = fetchExpected(testDirName);
+    var sourcePath = fetchSource(testDirName, 'source.djs');
     /**
      * Execute the binary and capture the stdout
      */
-    const { exec } = require('child_process');
-    const child = exec(`node ./bin/dogescript.js ${sourcePath}`,
-      (err, stdout, stderr) => {
-        if(err)
-        {
-          console.error(`exec error: ${err}`);
-          return;
-        }
-      }
-    );
-
-    // stream the output
-    var readable = child.stdout;
-    readable.setEncoding('utf8');
-
-    // when the process sends us data, validate they look the same
-    readable.on('data', function(chunk) {
-
-      var actual = cleanCRLF(chunk.toString());
-      test(`transpilation of ${path.basename(dir)} should work`, function (t) {
-        t.plan(1);
-
-        t.equal(actual,expected);
-      });
-
-    });
+     exec(`node ./bin/dogescript.js ${sourcePath}`, { encoding: 'UTF-8' }, (error, stdout, stderr) => {
+       if (error) {
+         t.fail(`exec error: ${error}`);
+         return;
+       }
+       var actual = cleanCRLF(stdout);
+       t.equal(expected, actual);
+     });
   });
-});
+}
+
+runTest('escaped-quotes');
+runTest('iota');
