1538a696e658d4e9405446d624de400099cde1cc
AnEmortalKid <janmonterrubio@gmail.com>
2019-07-09 21:16:22 -0500

    syntax tests

--- test/syntax.test.js
+++ test/syntax.test.js
@@ -57,3 +57,19 @@ test("'sit' without function name throws syntax error", function (t) {
   t.ok(err instanceof SyntaxError, 'throws SyntaxError');
   t.ok(msg.includes("Expected setter name but got nothing. Allowed construct: sit [name] with [arg]."), 'Contains correct message');
 });
+
+test("'sit' without 'with' throws syntax error", function (t) {
+  t.plan(2);
+  var err = withInvalidSyntax('sit fnName');
+  const msg = err.message;
+  t.ok(err instanceof SyntaxError, 'throws SyntaxError');
+  t.ok(msg.includes("Expected: 'with' but got: 'undefined' from chain: []. Allowed construct: sit [name] with [arg]."), 'Contains correct message');
+});
+
+test("'sit' without an argument throws a syntax error", function (t) {
+  t.plan(2);
+  var err = withInvalidSyntax('sit fnName with');
+  const msg = err.message;
+  t.ok(err instanceof SyntaxError, 'throws SyntaxError');
+  t.ok(msg.includes("Expected setter argument but got nothing. Allowed construct: sit [name] with [arg]."), 'Contains correct message');
+});
