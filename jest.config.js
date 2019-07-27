16fc153767e85905a35f1b268c39f22d9f840c7b
Cassidy Bridges <cbridges@greenbits.com>
2019-07-27 15:27:27 -0700

    remove tape/tapdot, implement jest

--- jest.config.js
+++ jest.config.js
@@ -27,9 +27,10 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/test"
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
