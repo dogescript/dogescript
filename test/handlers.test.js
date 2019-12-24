4857f42ead51c2b39c7d6287284c4990a2143ae6
Jan Monterrubio <janmonterrubio@gmail.com>
2019-12-23 18:45:37 -0600

    pull non statement funcs (#248)

--- test/handlers.test.js
+++ test/handlers.test.js
@@ -1,35 +1,86 @@
-import operatorHandlers from '../lib/handlers/operatorHandlers'
+import operatorHandlers from "../lib/handlers/operatorHandlers";
+import statementHandlers from "../lib/handlers/statementHandlers";
 
 describe("operatorHandlers", function() {
+  describe("handleBinaryOperator", function() {
+    it("throws an error when called with an unsupported token", function() {
+      var parseContext = {
+        tokens: ["wow"],
+        inputTokens: ["wow"],
+        input: "wow"
+      };
+      var expectedMsg =
+        'Expected one of [bigger,biggerish,smaller,smallerish,and,or,not] but got wow. Parsed tokens [wow] from input "wow"';
+      var test = function() {
+        return operatorHandlers.handleBinaryOperator(parseContext);
+      };
+      expect(test).toThrow(new Error(expectedMsg));
+    });
+  });
 
-    describe("handleBinaryOperator", function() {
-        it("throws an error when called with an unsupported token", function () {
-            var parseContext = {
-                tokens: ['wow'],
-                inputTokens: ['wow'],
-                input: "wow"
-            }
-            var expectedMsg = "Expected one of [bigger,biggerish,smaller,smallerish,and,or,not] but got wow. Parsed tokens [wow] from input \"wow\""
-            var test = function() {
-                return operatorHandlers.handleBinaryOperator(parseContext);
-            };
-            expect(test).toThrow(new Error(expectedMsg));
-        });
-        });
-    
+  describe("handleAssignmentOperator", function() {
+    it("throws an error when called with an unsupported token", function() {
+      var parseContext = {
+        tokens: ["wow"],
+        inputTokens: ["wow"],
+        input: "wow"
+      };
+      var expectedMsg =
+        'Expected one of [more,less,lots,few,is,as] but got wow. Parsed tokens [wow] from input "wow"';
+      var test = function() {
+        return operatorHandlers.handleAssignmentOperator(parseContext);
+      };
+      expect(test).toThrow(new Error(expectedMsg));
+    });
+  });
 
-    describe("handleAssignmentOperator", function() {
-        it("throws an error when called with an unsupported token", function () {
-          var parseContext = {
-              tokens: ['wow'],
-              inputTokens: ['wow'],
-              input: "wow"
-          }
-          var expectedMsg = "Expected one of [more,less,lots,few,is,as] but got wow. Parsed tokens [wow] from input \"wow\""
-          var test = function() {
-              return operatorHandlers.handleAssignmentOperator(parseContext);
-            };
-          expect(test).toThrow(new Error(expectedMsg));
-        });
-      });
+  describe("handleMaybe", function() {
+    it("throws an error when called with an unsupported token", function() {
+      var parseContext = {
+        tokens: ["wow"],
+        inputTokens: ["wow"],
+        input: "wow"
+      };
+      var expectedMsg =
+        "Invalid parse state! Expected: 'maybe' but got: 'wow' from chain: [wow]. Parsed tokens [wow] from input \"wow\"";
+      var test = function() {
+        return operatorHandlers.handleMaybe(parseContext);
+      };
+      expect(test).toThrow(new Error(expectedMsg));
+    });
+  });
+});
+
+describe("statementHandlers", function() {
+  describe("handleNext", function() {
+    it("throws an error when called with an unsupported token", function() {
+      var parseContext = {
+        tokens: ["wow"],
+        inputTokens: ["wow"],
+        input: "wow"
+      };
+      var expectedMsg =
+        "Invalid parse state! Expected: 'next' but got: 'wow' from chain: [wow]. Parsed tokens [wow] from input \"wow\"";
+      var test = function() {
+        return statementHandlers.handleNext(parseContext);
+      };
+      expect(test).toThrow(new Error(expectedMsg));
+    });
+  });
+
+  describe("handleObj", function() {
+    it("throws an error when called with an unsupported token", function() {
+      var parseContext = {
+        tokens: ["wow"],
+        inputTokens: ["wow"],
+        input: "wow"
+      };
+      var expectedMsg =
+        "Invalid parse state! Expected: 'obj' but got: 'wow' from chain: [wow]. Parsed tokens [wow] from input \"wow\"";
+      var test = function() {
+        return statementHandlers.handleObj(parseContext);
+      };
+      expect(test).toThrow(new Error(expectedMsg));
+    });
+  });
 });
